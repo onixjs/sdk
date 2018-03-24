38cef4ad625adcf1dacacb38f3ddc9e1dd2596cd
Miguel Serrano <miguelangelserrano4@gmail.com>
2018-03-23 19:16:24 -0600

    Nativescript adapter

---
+++ src/core/nativescript.adapters.ts
@@ -0,0 +1,59 @@
+import * as http from 'http';
+import { IWS, IHTTP } from '../interfaces';
+require('nativescript-websockets');
+const WS = WebSocket;
+/**
+ * @namespace Nativescript
+ * @author Miguel Serrano
+ * @description This namespace will provide adapters for the
+ * SDK to work in a Nativescript environment,
+ */
+export namespace Nativescript {
+  /**
+   * @class WebSocket
+   * @author Miguel Serrano
+   * @description This class is used when the SDK is running in a
+   * Nativescript Environment.
+   */
+  export class WebSocket implements IWS {
+    private connection;
+    connect(url: string) {
+      this.connection = new WS(url, []);
+    }
+    on(name: string, callback) {
+      switch (name) {
+        case 'message':
+          this.connection.addEventListener(name, (evt) => {
+            callback(evt.data);
+          });
+          break;
+        default:
+          throw new Error(
+            `ONIX Client: WebSocket event ${name} is not implemented.`,
+          );
+      } 
+    }
+    send(something: string) {
+      this.connection.send(something);
+    }
+    open(callback) {
+      this.connection.addEventListener('open', callback);
+    }
+  }
+  /**
+   * @class HTTP
+   * @author Miguel Serrano
+   * @description This class is used when the SDK is running in a
+   * Nativescript Environment.
+   */
+  export class HTTP implements IHTTP {
+    async get(url: string): Promise<object> {
+      return new Promise<object>((resolve, reject) => {
+        http.request({method: 'GET', url}).then( res => {
+          resolve(res.content.toJSON());
+          // Rehect on error
+        }, e => reject(e));
+      });
+    }
+  }
+}
